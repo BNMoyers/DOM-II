933d10a88705b24ff4130060b0b5bfe29537a230
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-06-18 15:09:27 -0500

    event #1 complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
 // Your code goes here
+
+//1. `mouseover`
+/* animate header image*/
+
+//2 .`keydown`
+/*offer help*/
+const wholePage = document.querySelector('body');
+wholePage.addEventListener('keydown', event=> {
+    return window.confirm("Would you like to chat with an agent?");
+})
+//3. `wheel`
+
+
+
+//4. `drag / drop`
+
+
+
+//5. `load`
+
+
+
+//6. `focus`
+
+
+//7. `resize`
+
+
+
+//8. `scroll`
+
+
+
+//9. `select`
+
+
+
+//10. `dblclick`
